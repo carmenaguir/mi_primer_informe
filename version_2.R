35e3126e9e49d5383589f600dee05b9ce10e0a5e
carmenaguir <carmenaguir@gmail.com>
2024-11-27 20:11:48 +0100

    Añado versión de versiones

---
+++ version_2.R
@@ -0,0 +1,19 @@
+
+#Código generado en RStudio
+version
+
+system("git config --global user.name 'carmenaguir'")
+system("git config --global user.email 'carmenaguir@gmail.com'")
+
+system("git config --global --list")
+system("notepad ~/.gitconfig")
+
+system("git config --global --list")
+
+# Para eliminar 
+system("git config --global --unset user.name")
+
+system("git config --global --list")
+system("git config --global --unset-all user.name")
+
+system("git config --list")
